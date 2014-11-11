a5110a57a895eaf45ade6b8ec6a799ef9d9ce682
ioud0001 <ioud0001@algonquinlive.com>
2014-11-10 21:21:51 -0500

    Updates to the original

--- js/main.js
+++ js/main.js
@@ -3,5 +3,13 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init(){
 	//startup script
+	var adults;
+	$("#num_adults").click(function(){
+		adults = 180 * $("#num_adults").val();
+		//alert($("#num_adults").val());
+		$("#price").val(adults);
+		$(".ui-slider-handle .ui-btn .ui-shadow").val(adults);
+	});
+	
 	
 }
